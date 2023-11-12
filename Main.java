5168e60b6a3464f8f7ff7e6cb931a1c475806f36
Will-I-Am-Ham <142340595+Will-I-Am-Ham@users.noreply.github.com>
2023-11-11 22:07:29 -0800

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,28 @@
+public class Main // DONE can submit
+{
+  public static void main(String[] args)
+  {
+    //Step 1
+    System.out.println(MediaLib.getOwner() + "'s Library");
+
+    //Step 8
+    MediaLib.changeOwner("William");
+    System.out.println(MediaLib.getOwner() + "'s Library");
+
+    MediaLib books = new MediaLib();
+    MediaLib movies = new MediaLib();
+
+    Book B = new Book("cats","auth");
+    books.addBook(B);
+    Book M = new Book("cats","auth");
+    movies.addBook(M);
+
+    //Step 17
+    System.out.println(books.getNumEntries()); 
+
+    books.addBook(new Book("dogs","auth"));
+    books.addBook(new Book("dogs","auth"));
+
+    System.out.println(books.getNumEntries()); //Step 22
+  }
+}
---
+++ MediaLib.java
@@ -0,0 +1,55 @@
+public class MediaLib {
+  private Book book;
+  public static String owner = "PLTW";
+
+  // Step 14
+  private static int numEntries = 0;
+
+  public void addBook(Book b) {
+    if (book != null) {
+      System.out.println("already book in here"); // Step 24
+      return;
+    }
+    numEntries++; // Step 15
+    book = b;
+  }
+
+  // new step 18-22
+  /*
+   * test scenario
+   * public void testBook(Book tester)
+   * {
+   * tester.setTitle("Animal Farm");
+   * // show state chagne with one of the following
+   * System.out.println(" in test: " + tester);
+   * System.out.println(" in test: " + tester.getTitle());
+   * }
+   */
+
+  public String toString() {
+    String info = "";
+
+    // Step 5, avoid crashing by testing that book is not null before using its
+    // toString method
+    if (book != null)
+      info += "Book: " + book + "\n";
+
+    return info;
+  }
+
+  // Step 1
+  public static String getOwner() {
+    return owner;
+  }
+
+  // Step 8
+  public static void changeOwner(String newOwner) {
+    owner = newOwner;
+  }
+
+  // Step 14
+  public int getNumEntries() {
+    System.out.println("Test: Owner is " + owner);
+    return numEntries;
+  }
+}
